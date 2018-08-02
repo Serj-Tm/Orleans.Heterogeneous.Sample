f9340a561854637a1b864b97c363e378db995468
- <->
2018-08-02 20:25:17 +0300

    add projects

---
+++ Orleans.Heterogeneous.Server1c/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Orleans.Heterogeneous.Server1c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
